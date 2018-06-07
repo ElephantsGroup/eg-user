2d9748dbc2cda0611d0909d1e113b00a46c99b42
JalalJaberi <j_jaberi@yhoo.com>
2018-06-08 01:50:33 +0430

    added auth_user_management migration

---
+++ migrations/m180607_205302_add_user_management.php
@@ -0,0 +1,85 @@
+<?php
+
+use yii\db\Migration;
+use yii\db\Query;
+
+/**
+ * Class m180607_205302_add_user_management
+ */
+class m180607_205302_add_user_management extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+		$db = \Yii::$app->db;
+		$query = new Query();
+        if ($db->schema->getTableSchema("{{%auth_item}}", true) !== null)
+		{
+			if (!$query->from('{{%auth_item}}')->where(['name' => '/user/admin/*'])->exists())
+				$this->insert('{{%auth_item}}', [
+					'name'			=> '/user/admin/*',
+					'type'			=> 2,
+					'created_at'	=> time(),
+					'updated_at'	=> time()
+				]);
+			if (!$query->from('{{%auth_item}}')->where(['name' => 'user_management'])->exists())
+				$this->insert('{{%auth_item}}', [
+					'name'			=> 'user_management',
+					'type'			=> 2,
+					'created_at'	=> time(),
+					'updated_at'	=> time()
+				]);
+			if (!$query->from('{{%auth_item}}')->where(['name' => 'user_manager'])->exists())
+				$this->insert('{{%auth_item}}', [
+					'name'			=> 'user_manager',
+					'type'			=> 2,
+					'created_at'	=> time(),
+					'updated_at'	=> time()
+				]);
+			if (!$query->from('{{%auth_item}}')->where(['name' => 'administrator'])->exists())
+				$this->insert('{{%auth_item}}', [
+					'name'			=> 'administrator',
+					'type'			=> 2,
+					'created_at'	=> time(),
+					'updated_at'	=> time()
+				]);
+		}
+        if ($db->schema->getTableSchema("{{%auth_item_child}}", true) !== null)
+		{
+			if (!$query->from('{{%auth_item_child}}')->where(['parent' => 'user_management', 'child' => '/user/admin/*'])->exists())
+				$this->insert('{{%auth_item_child}}', [
+					'parent'	=> 'user_management',
+					'child'		=> '/user/admin/*'
+				]);
+			if (!$query->from('{{%auth_item_child}}')->where(['parent' => 'user_manager', 'child' => 'user_management'])->exists())
+				$this->insert('{{%auth_item_child}}', [
+					'parent'	=> 'user_manager',
+					'child'		=> 'user_management'
+				]);
+			if (!$query->from('{{%auth_item_child}}')->where(['parent' => 'administrator', 'child' => 'user_manager'])->exists())
+				$this->insert('{{%auth_item_child}}', [
+					'parent'	=> 'administrator',
+					'child'		=> 'user_manager'
+				]);
+		}
+        if ($db->schema->getTableSchema("{{%auth_assignment}}", true) !== null)
+		{
+			if (!$query->from('{{%auth_assignment}}')->where(['item_name' => 'administrator', 'user_id' => 1])->exists())
+				$this->insert('{{%auth_assignment}}', [
+					'item_name'	=> 'administrator',
+					'user_id'	=> 1,
+					'created_at' => time()
+				]);
+		}
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+		// it's not safe to remove auth data in migration down
+    }
+}
